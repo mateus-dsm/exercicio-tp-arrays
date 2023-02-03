741c4677bb9b00ea62cc9ec9da33fa452bbd69a0
mateus-dsm <116819046+mateus-dsm@users.noreply.github.com>
2023-02-03 16:57:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    public static int[] retornaRanks(int[] pontuacoes)
+    {
+        int[] ranks = new int[pontuacoes.Length];
+        for(int i = 0; i < pontuacoes.Length; i++)
+        {
+            int rank = 1;
+            foreach (int elemento in pontuacoes)
+            {
+                if(elemento > pontuacoes[i]) rank++;
+            }
+            ranks[i] = rank;
+        }
+        return ranks;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] ints = { 1, 2, 2, 1 };
+        int[] ints2 = retornaRanks(ints);
+        foreach (int elemento in ints2) Console.WriteLine(elemento);
+    }
+}
